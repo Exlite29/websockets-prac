aff1675407c17267b13366003bef16e80e242e1a
Ariel Agdon <arielagdon@yahoo.com>
2023-11-23 19:07:59 +0800

    updated from ws to websocket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
-const ws = require("ws");
-const server = new ws.Server({ port: 3000 });
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-server.on("connection", (socket) => {
-  socket.on("message", (message) => {
-    const b = Buffer.from(message);
-    console.log(b.toString());
-    socket.send(`${message}`);
+const generateRandomName = () => {
+  const adjectives = ["Happy", "Funny", "Clever", "Silly", "Witty"];
+  const nouns = ["Cat", "Dog", "Fish", "Bird", "Elephant"];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return `${adjective}${noun}`;
+};
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  const randomName = generateRandomName();
+  console.log(`User ${randomName} (${socket.id}) connected`);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", `${randomName}: ${data}`);
   });
 });
+
+httpServer.listen(3500, () => console.log("listening on port 3500"));
